d8ba9a3fdf8a1e40f6dc35d748ef1a7b942c3a20
nka1024 <nka1024@gmail.com>
2019-06-01 01:24:02 +0700

    exosuit and pump lore

--- src/Dialogs.ts
+++ src/Dialogs.ts
@@ -36,6 +36,58 @@ export class Dialogs {
     ]},
   ];
 
+
+  //
+  // Pomp filter
+  // 
+
+  public static pompFilterStart: DialogLine[] = [
+    { a: DialogActorID.Unknown, m: 'Старая нососно-фильтровальная подстанция. Поршни застыли в неподвижном состоянии. Она явно требует ремонта, но возможно у вас получится запустить систему хотя бы в аварийном режиме.'},
+    { a:  DialogActorID.Unknown, m: '', o: [
+      { m:'Ничего не делать' },
+      { m:'Начать процедуру отладки', e: StoryEvent.BattleStart }
+    ]},
+  ];
+  
+  public static pompFilterDefeat: DialogLine[] = [
+    { a: DialogActorID.Unknown, m: 'Попытка запуска системы фильтрации не удалась'},
+  ];
+
+  public static pompFilterVictory: DialogLine[] = [
+    { a: DialogActorID.Unknown, m: 'Вам удалось запустить систему фильтрации в аварийном режиме. Пусть не скоро, но вода очистится и запах гнили уйдет'},
+  ];
+
+  public static pompFilterRepeat: DialogLine[] = [
+    { a: DialogActorID.Unknown, m: 'Насосно-фильтровальная подстанция гудит и громыхает поршнями, проводя через себя кубометры загрязненной воды'},
+  ];
+
+
+  //
+  // Exosuit
+  // 
+
+  public static exosuitStart: DialogLine[] = [
+    { a: DialogActorID.Unknown, m: 'Тело брошеного экзоскелета по пояс ушло под воду. Большая часть корпуса покрыта коррозиями и остается лишь догадываться в каком состоянии находится нижняя половина агрегата. '},
+    { a: DialogActorID.Unknown, m: 'Несмотря на внешние повреждения, некоторые индикаторы консоли все еще мигают. Значит ядро системы управления пока не отключилось. В блоке памяти экзоскелета может быть что-то интересное.'},
+    { a: DialogActorID.Unknown, m: '', o: [
+      { m:'Ничего не делать' },
+      { m:'Подключиться к консоли', e: StoryEvent.BattleStart }
+    ]},
+  ];
+  
+  public static exosuitDefeat: DialogLine[] = [
+    { a: DialogActorID.Unknown, m: 'В доступе к системе экзоскелета отказано'},
+  ];
+
+  public static exosuitVictory: DialogLine[] = [
+    { a: DialogActorID.Unknown, m: 'Добравшись до ядра системы экзоскелета вы нашли лог последних задач. Среди них "Транспортировка образцов протогенеза" и "Аварийная эвакуация персонала". '},
+  ];
+
+  public static exosuitRepeat: DialogLine[] = [
+    { a: DialogActorID.Unknown, m: 'Экзоскелет в крайне плохом состоянии.'},
+  ];
+
+  
   
 
 }
